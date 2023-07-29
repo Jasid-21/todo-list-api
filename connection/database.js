e36dabdb3922cf3bec10ed08cdc3f00e8d9a5340
Jasid Martinez <jasidmartinez21@gmail.com>
2023-07-29 11:59:37 -0500

    Change: From createConnection to createPool to manage shared databases connection

--- connection/database.js
+++ connection/database.js
@@ -2,20 +2,11 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.PORT,
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD
-});
-
-connection.connect((error) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-
-    console.log("Database connected successfully!");
 });
 
 module.exports = connection;
