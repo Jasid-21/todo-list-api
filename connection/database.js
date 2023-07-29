d312431db2bfd94021223fe4c42fe729ff24703f
Jasid Martinez <jasidmartinez21@gmail.com>
2023-07-29 12:08:54 -0500

    Change: From process.env.PORT to process.env.HOST

--- connection/database.js
+++ connection/database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createPool({
-    host: process.env.PORT,
+    host: process.env.HOST,
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD
